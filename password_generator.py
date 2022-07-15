1d06289c93e88b4214fc2040ada24c77de9311dd
Loucas <109354631+LoukyyZ@users.noreply.github.com>
2022-07-15 15:31:54 +0200

    Add files via upload

---
+++ password_generator.py
@@ -0,0 +1,34 @@
+import random
+
+#caractere
+lettre = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+nombre = "123456789"
+symboles = "§£$%@#{}[]()!?"
+caractere = lettre + lettre.lower() + nombre + symboles
+caractere_no_symbols = lettre + lettre.lower() + nombre
+
+
+while True:
+    longeurmdp = int(input("Entrez la longeur du mot de passe / Enter password length :"))
+    nombredemdp = int(input("Entrez le nombre de mot de passe a afficher / Enter the number of passwords to display :"))
+    caractere_speciaux_ask = str(input("Dois-je mettre des caracteres spéciaux dans le mot de passe / Do I have to put special characters in the password ?:"))
+     
+    if caractere_speciaux_ask == 'y':
+        for i in range(0,nombredemdp):
+            mdp = ""
+            for i in range(0,longeurmdp):
+                cmdp = random.choice(caractere)
+                mdp = mdp +cmdp
+            print('Votre mot de passe est :', mdp)
+
+    elif caractere_speciaux_ask == 'n':
+        for i in range(0,nombredemdp):
+            mdp = ""
+            for i in range(0,longeurmdp):
+                cmdp = random.choice(caractere_no_symbols)
+                mdp = mdp +cmdp
+            print('Votre mot de passe est :', mdp)
+    elif caractere_speciaux_ask !='oui' or 'non':
+        print ("ERREUR")
+            
+      
